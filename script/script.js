a0bc4581c40aa1c4f9d71a76dd06867fbcd4cf32
simfrisk <simoncarlfrisk@gmail.com>
2025-02-21 19:01:10 +0100

    Buttons and darkmode

--- script/script.js
+++ script/script.js
@@ -5,3 +5,11 @@
 // function previousMovies(container, scrollDistance) {
 //   container.scrollBy({ left: -scrollDistance, behavior: "smooth" });
 // }
+
+const darkModeToggle = document.querySelector("#temp")
+const body = document.querySelector("#body")
+
+darkModeToggle.addEventListener("click", () => {
+  console.log("nice")
+  body.classList.toggle("dark-mode")
+}) 
