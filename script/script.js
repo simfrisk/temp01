d2727567b6b2ab412c51f4aa19503626c80a82b9
simfrisk <simoncarlfrisk@gmail.com>
2025-02-21 15:11:01 +0100

    started the site

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// function nextMovies(container, scrollDistance) {
+//   container.scrollBy({ left: scrollDistance, behavior: "smooth" });
+// }
+
+// function previousMovies(container, scrollDistance) {
+//   container.scrollBy({ left: -scrollDistance, behavior: "smooth" });
+// }
