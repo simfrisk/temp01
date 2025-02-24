06c6c65a621a3a859ed5644bac254064a717a661
simfrisk <simoncarlfrisk@gmail.com>
2025-02-24 15:51:00 +0100

    started the site

--- script/script.js
+++ script/script.js
@@ -12,4 +12,25 @@ const body = document.querySelector("#body")
 darkModeToggle.addEventListener("click", () => {
   console.log("nice")
   body.classList.toggle("dark-mode")
-}) 
+})
+
+
+// search
+
+const getSearchInput = document.querySelector("#search");
+
+getSearchInput.addEventListener("input", (event) => {
+  console.log(event.target.value); // Logs the current input value
+});
+
+
+// Testing
+
+const birthYear = 1992
+
+const age = (birthYear) => {
+  birthYear = 2025 - birthYear
+  console.log(`hello sir you are ${birthYear} years old`)
+};
+
+age(birthYear) // Call the correct function
